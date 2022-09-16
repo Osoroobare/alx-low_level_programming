3eff3fdacb942cd9759af041e3aaf4246a3dea43
Osoro Brian <brianosoro40@gmail.com>
2022-09-16 11:11:28 +0300

    task2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * _putchar - prints character to stdout
+ * @c: character to be printed
+ * Return: On success 1.
+ * on error, -1
+ */
 #include <stdio.h>
+int _putchar(char c);
 int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
 
