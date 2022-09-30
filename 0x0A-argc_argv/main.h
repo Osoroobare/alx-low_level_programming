d053bd1fb8e2583257b7ef471ba4383255179cb5
Osoro Brian <brianosoro40@gmail.com>
2022-09-30 16:13:44 +0300

    C - argc, argv tasks

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - prints character to stdout
+ *  @c: character to be printed
+ *  Return: 1 on success
+ *  -1 on error
+ */
+#include<stdio.h>
+int _putchar(char c);
+int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[]);
+#endif /* MAIN_H */
